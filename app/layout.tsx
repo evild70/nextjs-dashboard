9569f4f978aa68664c9d570409e8c4cb23273fd7
Dennis Lahay <dlahay@gmail.com>
2024-03-03 16:51:05 -0600

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
+import '@/app/ui/global.css';
+import {
+    inter, lusitana
+} from '@/app/ui/fonts';
+
 export default function RootLayout({
-  children,
+    children,
 }: {
+  // eslint-disable-next-line no-undef
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
